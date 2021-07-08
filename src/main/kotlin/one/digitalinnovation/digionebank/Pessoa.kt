4762b52aff604b966086befef7879f2514983b0d
ViniFreitas23 <vinicius.freitas.correia@hotmail.com>
2021-07-08 13:12:13 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Vinicius"
-
-    var cpf: String = "123.123.123-12"
-
-    private set
-}
-
-fun main(){
-    val vinicius = Pessoa()
-
-    println(vinicius.nome)
-    println(vinicius.cpf)
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
